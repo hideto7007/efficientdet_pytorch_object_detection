39eead5fa189c4361db819aca2d15212af10b4f8
hideto tsuzuki <ma_kux@icloud.com>
2023-01-01 21:00:14 +0900

    yamlファイル追加

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 
 echo 'train.pyバッチ実行'
-your_project_name="./datasets/library_data/"
-python train.py -c 1 -p $your_project_name --batch_size 8 --lr 1e-5
+your_project_name="library_data"
+python train.py -c 1 -p $your_project_name --batch_size 2 --lr 1e-5
 echo 'train.pyバッチ終了'
 echo $?
