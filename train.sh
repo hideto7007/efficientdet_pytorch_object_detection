956f633bb9817985a170f569d9705089a1c1252e
hideto tsuzuki <ma_kux@icloud.com>
2023-01-01 09:51:54 +0900

    実行スクリプト変更

--- train.sh
+++ train.sh
@@ -2,6 +2,7 @@
 
 
 echo 'train.pyバッチ実行'
-python train.py
+your_project_name="./datasets/library_data/"
+python train.py -c 1 -p $your_project_name --batch_size 8 --lr 1e-5
 echo 'train.pyバッチ終了'
 echo $?
